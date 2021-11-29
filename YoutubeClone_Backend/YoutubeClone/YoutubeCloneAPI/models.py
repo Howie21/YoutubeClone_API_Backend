2ba4ccc62ca0cd2211188f3678b5acf0193710d1
Howie21 <bryce9643@gmail.com>
2021-11-29 11:21:28 -0500

    Added structure of Comment and Reply Models

--- YoutubeClone_Backend/YoutubeClone/YoutubeCloneAPI/models.py
+++ YoutubeClone_Backend/YoutubeClone/YoutubeCloneAPI/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 class Comment(models.Model):
     comment = models.CharField(max_length=500)
-    likes = models.IntegerField()
-    dislikes = models.IntegerField()
-    
+    likes = models.IntegerField(default=0 )
+    dislikes = models.IntegerField(default=0)
+
+class Reply(models.Model):
+    comment = models.CharField(max_length=500)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    ForeignKey = models.ForeignKey(Comment, null=True, on_delete=models.CASCADE)
